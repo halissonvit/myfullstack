f6815c885e4ee7595ba9d9e9226b2480201d6212
Halisson Vitor <halissonvit@gmail.com>
2015-05-27 02:50:06 +0000

    implementing in-place editable element

---
+++ javascripts/components/editable.jsx
@@ -0,0 +1,13 @@
+var Editable = React.createClass({
+  componentDidMount: function() {
+    var el = React.findDOMNode(this);
+    $(el).editable();
+  },
+  render: function() {
+    var text = this.props.children;
+    var elementName = this.props.element ? this.props.element : 'span';
+    var element = React.createElement(elementName, this.props, text);
+
+    return element;
+  }
+});
