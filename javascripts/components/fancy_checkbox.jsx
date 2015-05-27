d39649f31c5d08292e785e122befeeb2b1383404
Halisson Vitor <halissonvit@gmail.com>
2015-05-27 18:10:48 +0000

    Fixing indentation

--- javascripts/components/fancy_checkbox.jsx
+++ javascripts/components/fancy_checkbox.jsx
@@ -1,16 +1,16 @@
 var FancyCheckbox = React.createClass({
-    render: function() {
-        var id = this.props.id ? this.props.id : "fancy-checkbox";
-        var name = this.props.name ? this.props.name : id;
-        var label = this.props.label ? this.props.label : 'checked?';
-        var className = this.props.className ? this.props.className : 'form-group';
+  render: function() {
+    var id = this.props.id ? this.props.id : "fancy-checkbox";
+    var name = this.props.name ? this.props.name : id;
+    var label = this.props.label ? this.props.label : 'checked?';
+    var className = this.props.className ? this.props.className : 'form-group';
 
-        return (
-            <div className={className}>
-                <input type="checkbox" id={id} className="switch" name={name} />
-                <label htmlFor={id}>{label}</label>
-            </div>
-        );
-    }
+    return (
+      <div className={className}>
+      <input type="checkbox" id={id} className="switch" name={name} />
+      <label htmlFor={id}>{label}</label>
+      </div>
+    );
+  }
 });
 
