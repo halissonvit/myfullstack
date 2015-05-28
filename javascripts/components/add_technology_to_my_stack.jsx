2e8d2c9e1b4269374a47d88bed7c3d3442f21eae
Halisson Vitor <halissonvit@gmail.com>
2015-05-28 00:33:10 +0000

    fixing indentation

--- javascripts/components/add_technology_to_my_stack.jsx
+++ javascripts/components/add_technology_to_my_stack.jsx
@@ -1,17 +1,18 @@
 var AddTechnologyToMyStack = React.createClass({
-    render: function() {
-        var wrapperClass = this.props.wrapperClass ? this.props.wrapperClass : 'wrapper';
-        var text = this.props.children ? this.props.children : 'submit';
-        var className = 'btn btn-primary btn-block';
+  render: function() {
+    var wrapperClass = this.props.wrapperClass ? this.props.wrapperClass : 'wrapper';
+    var text = this.props.children ? this.props.children : 'submit';
+    var className = 'btn btn-primary btn-block';
 
-        return (
-            <div className={wrapperClass}>
-                <div className="form-group">
-                    <label className="sr-only" htmlFor="technology">Choose a technology and add to your stack</label>
-                    <input type="text" className="form-control" id="technology" placeholder="Choose a technology and add to your stack..." />
-                </div>
-                <button className={className} type="submit">Add to my stack</button>
-            </div>
-        );
-    }
+    return (
+      <div className={wrapperClass}>
+        <div className="form-group">
+          <label className="sr-only" htmlFor="technology">Choose a technology and add to your stack</label>
+          <input type="text" className="form-control" id="technology" placeholder="Choose a technology and add to your stack..." />
+        </div>
+
+        <button className={className} type="submit">Add to my stack</button>
+      </div>
+    );
+  }
 });
