dc867d453bdb6a1a7783d85e46fdd7c2008f7eff
Halisson Vitor <halissonvit@gmail.com>
2015-05-30 01:44:33 +0000

    adding default props for add technology stack component

--- javascripts/components/add_technology_to_my_stack.jsx
+++ javascripts/components/add_technology_to_my_stack.jsx
@@ -1,13 +1,23 @@
 var AddTechnologyToMyStack = React.createClass({
+  getDefaultProps: function() {
+    return {
+      wrapperClass: 'wrapper',
+      inputWrapperClass: 'form-group',
+      labelClass: 'sr-only',
+      inputClass: 'form-control',
+      inputId: 'technology',
+      children: 'Choose a technology and add to your stack'
+    }
+  },
   render: function() {
     var wrapperClass = this.props.wrapperClass ? this.props.wrapperClass : 'wrapper';
     var text = this.props.children ? this.props.children : 'submit';
     var className = 'btn btn-primary btn-block';
 
     return (
-      <div className={wrapperClass}>
-        <div className="form-group">
-          <label className="sr-only" htmlFor="technology">Choose a technology and add to your stack</label>
+      <div className={this.props.wrapperClass}>
+        <div className={this.props.inputWrapperClass}>
+          <label className={this.props.labelClass} htmlFor={this.props.inputId}>{this.props.children}</label>
           <input type="text" className="form-control" id="technology" placeholder="Choose a technology and add to your stack..." />
         </div>
 
